4cc5c80d459005d09eb7a6c23deedc1395f41a2f
birney <birney@birneydeMacBook-Pro.local>
2017-04-13 17:15:26 +0800

    m

--- CloudChat/CloudChat/CCContactTableViewController.m
+++ CloudChat/CloudChat/CCContactTableViewController.m
@@ -73,7 +73,7 @@
         NSString* userId = self.dataSource[indexPath.section][indexPath.row];
         RCUserInfo* userInfo = [[RCIM sharedRCIM] getUserInfoCache:userId];
         cell.textLabel.text = userInfo.name;
-        cell.imageView.yy_imageURL = [NSURL URLWithString:userInfo.portraitUri];
+        //cell.imageView.yy_imageURL = [NSURL URLWithString:userInfo.portraitUri];
     }
     return cell;
 }
